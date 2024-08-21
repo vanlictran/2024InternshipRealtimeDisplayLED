10ca3c8ca60ca53f62abfd9286430ac3d81b5d02
NicolasZanin <nicolas.zanin@etu.univ-cotedazur.fr>
2024-08-21 14:14:21 +0700

    fix: delete file error

--- application_c_sharp/api_csharp_uplink/DB/InfluxDbSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace api_csharp_uplink.DB;
-
-public class InfluxDbSettings
-{
-    public string Url { get; init; } = "";
-    public string Token { get; init; } = "";
-    public string OrgName { get; init; } = "";
-    public string BucketName { get; init; } = "";
-}
